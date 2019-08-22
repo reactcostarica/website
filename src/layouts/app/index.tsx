ea5447ccfaa796a9192440d3df27175093320495
Kevin Wolf <hi@kevinwolf.me>
2019-08-22 15:31:18 -0600

    add framer motion

--- src/layouts/app/index.tsx
+++ src/layouts/app/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ThemeProvider } from 'emotion-theming'
 import { Global, css } from '@emotion/core'
 import normalize from 'emotion-normalize'
+import { motion } from 'framer-motion'
 
 import useViewport from 'hooks/viewport'
 import theme from 'config/theme'
@@ -16,6 +17,11 @@ export default function AppLayout({ children }: AppLayoutProps) {
   const { vh } = useViewport()
   const viewportHeight = vh(100)
 
+  const variants = {
+    hidden: { opacity: 0, scale: 0.5 },
+    visible: { opacity: 1, scale: 1 },
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Global
@@ -29,18 +35,24 @@ export default function AppLayout({ children }: AppLayoutProps) {
           }
         `}
       />
-      <Box
-        minHeight={viewportHeight}
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
-        opacity={viewportHeight === 0 ? 0 : 1}
-        p={2}
+
+      <motion.div
+        variants={variants}
+        initial="hidden"
+        animate={!viewportHeight ? 'hidden' : 'visible'}
       >
-        {children}
-        <Footer />
-      </Box>
+        <Box
+          minHeight={viewportHeight}
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          p={2}
+        >
+          {children}
+          <Footer />
+        </Box>
+      </motion.div>
     </ThemeProvider>
   )
 }
